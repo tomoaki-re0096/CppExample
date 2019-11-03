696aaf6733139bac5aa35b59244830fb66900a04
tomoaki-re0096 <banagher.rx02@gmail.com>
2019-11-04 00:39:24 +0900

    shared unique
    SmartPtrサンプルコード追加

--- Exsample/source/SmartPtr.cpp
+++ Exsample/source/SmartPtr.cpp
@@ -1,5 +1,5 @@
 #include "SmartPtr.h"
-
+#include <iostream>
 
 class SmartPtrImpl : public SmartPtr
 {
@@ -7,23 +7,50 @@ private:
     class TestClass
     {
     public:
-        TestClass()
+        TestClass(const char* msg)
         {
-
+            std::cout << "constructor:" << msg << std::endl;
         }
         ~TestClass()
         {
-
+            std::cout << "destructor" << std::endl;
         }
     };
+
     //! shared_ptr
     void sharedTest()
     {
-    
+        auto null_check = [](auto& ptr)
+        {
+            if (!ptr) { std::cout << "null" << std::endl; }
+        };
+        auto ptr = std::make_shared<TestClass>("shared");
+        {
+            auto copy = ptr;
+            null_check(ptr);//sharedはコピーが出来るのでptrは有効
+        }
+        std::cout << "end" << std::endl;
     }
     //! unique_ptr
     void uniqueTest()
     {
+        auto null_check = [](auto& ptr) 
+        {
+            if (!ptr) { std::cout << "null" << std::endl; }
+        };
+
+        auto ptr = std::make_unique<TestClass>("unique");
+        {
+            // コピーは出来ない。移行のみで、その時はstd::move
+            auto move = std::move(ptr);
+            null_check(ptr);
+        }
+
+        auto create_unique = []() {return std::make_unique<TestClass>("right"); };
+        //! この時はmoveは必要なし
+        auto right_num = create_unique();
+
+        std::cout << "end" << std::endl;
 
     }
     void main() override
--- Exsample/source/main.cpp
+++ Exsample/source/main.cpp
@@ -1,11 +1,16 @@
 
 #include "Cpp11.h"
 #include "Cpp17.h"
+#include "SmartPtr.h"
+
 int main()
 {
     auto cpp11 = Cpp11::create();
     cpp11->main();
     auto cpp17 = Cpp17::create();
     cpp17->main();
+
+    auto smart_ptr = SmartPtr::create();
+    smart_ptr->main();
     return 0;
 }
